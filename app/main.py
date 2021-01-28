181f9e54313ab3d7c968093c1eeb746ddd895409
Andre Lara <alosbh@gmail.com>
2021-01-28 17:07:29 -0300

    metrics

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
-host = open('/usr/src/app/hostname')
-for line in host:
-    print(line)
-print('tentei')
+import requests
+import json
+# host = open('/usr/src/app/hostname')
+# for line in host:
+#     print(line)
+# print('tentei')
+
+urlMetrics = 'http://brbelm0itqa01/AIOService_AIODashboard/Prodash/GetActualUserAttributes/'
+hostname = 'brbelrasp103'
+r = requests.get(urlMetrics+hostname)
+
+print(r.json())
